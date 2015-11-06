7e69676ba7812d8df62dc886908365df077b5e40
mollymerp <mollylloyd@gmail.com>
2015-11-05 16:39:20 -0800

    [refactor]: move mongolab key to secret location, set up mongolab account information and instance

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 // A gameplan-app project
 // =============================================================================
 
-var express = require('express'); // bring in express
-var bodyParser = require('body-parser'); // bring in body parser for parsing requests
-var router = require('./router.js'); // add link to our router file
-var session = require('express-session'); // to enable user sessions
-var passport = require('passport'); // auth via passport
-var FacebookStrategy = require('passport-facebook').Strategy; // FB auth via passport
-var cookieParser = require('cookie-parser'); // parses cookies
-var uriUtil = require('mongodb-uri'); // util for Mongo URIs
+var express = require('express');        // bring in express
+var bodyParser = require('body-parser');  // bring in body parser for parsing requests
+var router = require('./router.js');  // add link to our router file
+var session = require('express-session');  // to enable user sessions
+var passport = require('passport');  // auth via passport
+var FacebookStrategy = require('passport-facebook').Strategy;  // FB auth via passport
+var cookieParser = require('cookie-parser');  // parses cookies
+var uriUtil = require('mongodb-uri');  // util for Mongo URIs
+var config = require('./config/dev-config.js');
 
 // SCHEMA / MODELS
 var User = require('./models/userModel.js');
@@ -17,9 +18,11 @@ var Site = require('./models/siteModel.js');
 var app = express(); // define our app using express
 var port = process.env.PORT || 8080; // set our port
 
+<<<<<<< 38d1766149c273502cb8c9c158765a9b93e745fd
 app.use(bodyParser.urlencoded({
   extended: true
 })); // use bodyParser() for req body parsing
+
 app.use(bodyParser.json());
 
 // AUTH INIT
@@ -39,6 +42,7 @@ passport.deserializeUser(function(obj, done) {
 
 
 // DATABASE
+
 var mongoose = require('mongoose'); // enable Mongoose for db
 var mongodbUri = 'mongodb://ryan:gaaame@ds049104.mongolab.com:49104/gaaame_db'; // our DB URI
 var mongooseUri = uriUtil.formatMongoose(mongodbUri); // formatting for Mongoose
@@ -81,16 +85,29 @@ console.log('Unbalanced magic is happening on port ' + port);
 
 
 // DB TESTING - keep this! uncomment to test if db is connected
-// var userCreate = Q.nbind(User.create, User);
-// var newUser = {
-//  'user_fb_id' : 12345,
-//  'username' : 'alex'
-// };
-// userCreate(newUser);
 
-// var siteCreate = Q.nbind(Site.create, Site);
-// var newSite = {
-//  'site_place_id' : 54321,
-//  'sitename' : 'JAMTOWN'
-// };
-// siteCreate(newSite);
+  // var Q = require('q');
+  // var userCreate = Q.nbind(User.create, User);
+  // var newUser = {
+  //  'user_fb_id' : 12345,
+  //  'username' : 'alex'
+  // };
+  // userCreate(newUser)
+  // .then(console.log("user created"));
+
+  // var findUsers = Q.nbind(User.find, User);
+  // findUsers({})
+  // .then(function (users) {
+  //   console.log("all users", users);
+  // })
+  // .catch(function (error){
+  //   console.error(error)
+  // });
+
+  // var siteCreate = Q.nbind(Site.create, Site);
+  // var newSite = {
+  //  'site_place_id' : 54321,
+  //  'sitename' : 'JAMTOWN'
+  // };
+  // siteCreate(newSite);
+
