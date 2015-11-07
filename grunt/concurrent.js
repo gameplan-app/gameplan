528b694048b7c5ce2fdf5442f3c9c1383f246a8a
Chris Puskar <cjpuskar@comcast.net>
2015-11-07 12:14:58 -0800

    [feat]: Add ability to concurrently run some grunt tasks

--- grunt/concurrent.js
+++ grunt/concurrent.js
@@ -1,6 +1,5 @@
 // Run tasks concurrently - Out-of-the-box Grunt will run each task one after the other, which can take a while depending on the amount and type of tasks you need to run. However, there are often tasks that are not dependent on other tasks which can be run at the same time.
 
-
 module.exports = {
 
   // Task options
