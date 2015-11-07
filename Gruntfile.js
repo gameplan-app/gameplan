14d0709ec039a8c3650ba47228e4af515d6a6f5e
Chris Puskar <cjpuskar@comcast.net>
2015-11-07 13:24:51 -0800

    [style]: Add comments for load-grunt-config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,48 +1,14 @@
-// Gruntfile.js
 
-// our wrapper function (required by grunt and its plugins)
-// all configuration goes inside this function
+
 module.exports = function(grunt) {
+  // tells how much time each task takes
+  require('time-grunt')(grunt);  
 
-  // =====================================================
-  // CONFIGURE GRUNT =====================================
-  // =====================================================
-  grunt.initConfig({
-
-    // get the configuration info from package.json
-    // this way we can use things like name and version (pkg.name)
-    pkg: grunt.file.readJSON('package.json'),
-    log: {
-      name: '<%= pkg.name %>', // name of project in package.json
-      version: '<%= pkg.version %>' // name of project version
-    }
-    // all our configuration will go here
-
-    // configure jshint to validate js files
-  //   jshint: {
-  //     options: {
-  //       // use jshint-stylish to make errors look & read good
-  //       reporter: require('jshint-stylish') 
-  //     },
-
-  //     build: ['Gruntfile.js', 'client/app/app.js', 'client/app/views/*.js', 'server/**/*.js']
-  //   }
-  // });
-
-  // =====================================================
-  // LOAD GRUNT PLUGINS ==================================
-  // =====================================================
-  // we can only load these if they are in our package.json
-  // make sure you have run npm install so our app can find these
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
-  grunt.registerMultiTask('log', 'Log Project details.', function() {
-    // because this uses the registerMultiTask function it runs
-    // grunt.log.writeln() for each attr in above log: {} obj
-    grunt.log.writeln(this.target + ': ' + this.data);
+  // If your grunt/ folder contains an aliases.yaml file,
+  // load-grunt-config will use that to define your task aliases
+  // like grunt.registerTask('default', ['jshint']);
+  require('load-grunt-config')(grunt, {
+    // tells load-grunt-config to use faster jitGrunt
+    jitGrunt: true
   });
 };
