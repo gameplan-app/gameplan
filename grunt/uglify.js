f9ee9724ff822e37dbca3e8b5fa9ef0880388e9e
Chris Puskar <cjpuskar@comcast.net>
2015-11-07 12:25:06 -0800

    [feat]: Add uglify to project

---
+++ grunt/uglify.js
@@ -0,0 +1,12 @@
+// Takes JS files and minifies them
+module.exports = {
+  all: {
+    files: [{
+      expand: true,
+      cwd: 'src/scripts',
+      src: '**/*.js',
+      dest: 'dist/scripts',
+      ext: '.min.js'
+    }]
+  }
+};
