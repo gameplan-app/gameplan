92877489daa197b2c856358bc1afa5685b8a34ef
Chris Puskar <cjpuskar@comcast.net>
2015-11-07 12:06:08 -0800

    [refactor]: Add Grunt folder with indiviudal task files for modularity

---
+++ grunt/concurrent.js
@@ -0,0 +1,35 @@
+// Run tasks concurrently - Out-of-the-box Grunt will run each task one after the other, which can take a while depending on the amount and type of tasks you need to run. However, there are often tasks that are not dependent on other tasks which can be run at the same time.
+
+
+module.exports = {
+
+  // Task options
+  options: {
+    limit: 3
+  },
+
+  // Dev tasks
+  devFirst: [
+    'clean',
+    'jshint'
+  ],
+  devSecond: [
+    'sass:dev',
+    'uglify'
+  ],
+
+  // Production tasks
+  prodFirst: [
+    'clean',
+    'jshint'
+  ],
+  prodSecond: [
+    'sass:prod',
+    'uglify'
+  ],
+
+  // Image Tasks
+  imgFirst: [
+    'imagein'
+  ]
+};
