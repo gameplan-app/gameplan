604e90e05f9a53507886e6d1d5a5c4c2e94cb6d6
Chris Puskar <cjpuskar@comcast.net>
2015-11-07 17:04:07 -0800

    [style]: Make gruntfile modular and update dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,12 +3,14 @@
 module.exports = function(grunt) {
   // tells how much time each task takes
   require('time-grunt')(grunt);  
-
+  // require('jit-grunt')(grunt)({
+  //   pluginsRoot: 'node_modules'
+  // });
   // If your grunt/ folder contains an aliases.yaml file,
   // load-grunt-config will use that to define your task aliases
   // like grunt.registerTask('default', ['jshint']);
   require('load-grunt-config')(grunt, {
     // tells load-grunt-config to use faster jitGrunt
-    jitGrunt: true
+    // jitGrunt: true
   });
 };
