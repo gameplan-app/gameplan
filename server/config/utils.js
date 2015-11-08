f704591d881799bbf3964182287c4951396628b0
Chris Puskar <cjpuskar@comcast.net>
2015-11-08 11:17:36 -0800

    [style]: Fixed syntax errors after running jshint

--- server/config/utils.js
+++ server/config/utils.js
@@ -158,7 +158,7 @@ function addRes(place, date, time,user) {
         console.error(err);
         res.status(400).send("error making reservation");
       }
-    })
+    });
 }
 
 exports.siteReserve = function(req, res) {
@@ -179,7 +179,7 @@ exports.siteReserve = function(req, res) {
           res.status(202).send("there is already a reservation at time" + time);
         }
       });
-  })
+  });
   
 };
 
@@ -187,7 +187,7 @@ exports.siteDayAvailability = function(req, res) {
   var findQuery = {
     'site_place_id': req.query.site_name,
     'reservations.date': moment(req.query.date, "MMDDYYYY")
-  }
+  };
   var res_length = req.query.res_length || 1;
   var reserved_hours = [];
   Site.find(findQuery).exec(function(err, result) {
@@ -198,7 +198,7 @@ exports.siteDayAvailability = function(req, res) {
     // console.log("result fron site day availability", result);
     if (result[0] !== undefined) {
       _.each(result[0].reservations, function(reservation) {
-        var i = _.indexOf(reserved_hours, reservation.time)
+        var i = _.indexOf(reserved_hours, reservation.time);
         if (i < 0) {
           reserved_hours.push(reservation.time);
         }
@@ -214,7 +214,7 @@ exports.siteDayAvailability = function(req, res) {
 exports.getAllUsers = function (req, res) {
   User.find({}, 'username photo emails', function (err, result) {
     res.status(200).send(result);
-  })
+  });
 };
 
 
